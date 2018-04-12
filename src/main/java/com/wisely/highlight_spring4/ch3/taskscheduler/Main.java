46f2cfa8aadf6a9c509dd04b124293d65d0eb133
test <test@test.com>
2018-04-12 16:29:12 +0800

    scheduler commit

---
+++ src/main/java/com/wisely/highlight_spring4/ch3/taskscheduler/Main.java
@@ -0,0 +1,13 @@
+package com.wisely.highlight_spring4.ch3.taskscheduler;
+
+import com.wisely.highlight_spring4.ch3.taskexecutor.TaskExecutorConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskExecutorConfig.class);
+
+
+
+    }
+}
